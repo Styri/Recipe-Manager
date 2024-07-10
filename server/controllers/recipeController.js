ccc1e4bb8271457b86728567f2dbc572f7ddde8e
Styri <styltia@hotmail.com>
2024-07-10 21:21:20 +0300

    Implement pagination to APIs to improve database perfomance and scaleability

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -17,8 +17,13 @@ const createRecipe = async (req, res) => {
 };
 
 const getAllRecipes = async (req, res) => {
+  const { page = 1 } = req.query;
+  const limit = 8;
+  const offset = (page - 1) * limit;
+
   try {
-    const allRecipes = await pool.query("SELECT * FROM recipes ORDER BY recipe_id");
+    const allRecipes = await pool.query("SELECT * FROM recipes ORDER BY recipe_id LIMIT $1 OFFSET $2",
+      [limit, offset]);
     res.json(allRecipes.rows);
   } catch (error) {
     logger.error(`Error fetching recipes: ${error.message}`);
@@ -27,8 +32,13 @@ const getAllRecipes = async (req, res) => {
 };
 
 const getFavoriteRecipes = async (req, res) => {
+  const { page = 1 } = req.query;
+  const limit = 8;
+  const offset = (page - 1) * limit;
+
   try {
-    const favoriteRecipes = await pool.query("SELECT * FROM recipes WHERE favorite = TRUE ORDER BY recipe_id");
+    const favoriteRecipes = await pool.query("SELECT * FROM recipes WHERE favorite = TRUE ORDER BY recipe_id LIMIT $1 OFFSET $2",
+      [limit, offset]);
     res.json(favoriteRecipes.rows);
   } catch (error) {
     logger.error(`Error fetching favorite recipes: ${error.message}`);
@@ -65,8 +75,14 @@ const deleteRecipe = async (req, res) => {
 };
 
 const sortRecipesByCategory = async (req, res) => {
+  const { page = 1 } = req.query;
+  const limit = 8;
+  const offset = (page - 1) * limit;
+
   try {
-    const sortedRecipes = await pool.query("SELECT * FROM recipes ORDER BY category");
+    const sortedRecipes = await pool.query("SELECT * FROM recipes ORDER BY category LIMIT $1 OFFSET $2",
+      [limit, offset]
+    );
     res.json(sortedRecipes.rows);
   } catch (error) {
     logger.error(`Error sorting recipes: ${error.message}`);
@@ -104,12 +120,14 @@ const getRecipeStats = async (req, res) => {
 };
 
 const searchRecipes = async (req, res) => {
-  const { name = "", description = "", category = "" } = req.query;
+  const { name = "", description = "", category = "", page = 1 } = req.query;
+  const limit = 8;
+  const offset = (page - 1) * limit;
 
   try {
     const searchResults = await pool.query(
-      "SELECT * FROM recipes WHERE name ILIKE $1 OR description ILIKE $2 OR category ILIKE $3",
-      [`%${name}%`, `%${description}%`, `%${category}%`]
+      "SELECT * FROM recipes WHERE name ILIKE $1 OR description ILIKE $2 OR category ILIKE $3 ORDER BY recipe_id LIMIT $4 OFFSET $5",
+      [`%${name}%`, `%${description}%`, `%${category}%`, limit, offset]
     );
     res.json(searchResults.rows);
   } catch (error) {
